44651efd9eb07b77d64a655436da83ff0f9cdca8
vinicius <vinicius.savordelli@gmail.com>
2023-02-20 18:33:28 -0300

    feat: implements prescription parser

---
+++ backend/src/document_parsers/prescription_parser.py
@@ -0,0 +1,60 @@
+from generic_parser import generic_parser
+import re
+
+
+class prescription_parser(generic_parser):
+    def __init__(self, text):
+        self.text = text
+
+    def get_doctor_info(self):
+        return self.get_from_text("Dr(.*)Name", self.text, re.DOTALL)
+
+    def get_patient_name(self):
+        return self.get_from_text("Name:(.*)Date", self.text)
+
+    def get_address(self):
+        return self.get_from_text("Address[^\n]", self.text)
+
+    def get_prescription(self):
+        return self.get_from_text(
+            "Address[^\n]*(.*)Directions", self.text, flags=re.DOTALL
+        )
+
+    def get_directions(self):
+        return self.get_from_text(
+            "Directions:(.*)Refill", self.text, flags=re.DOTALL
+        )
+
+    def get_refill(self):
+        return self.get_from_text("Refill:(.*)", self.text)
+
+    def parse(self):
+        return {
+            "Doctor Info": self.get_doctor_info(),
+            "Patient Name": self.get_patient_name(),
+            "Address": self.get_address(),
+            "Prescription": self.get_prescription(),
+            "Directions": self.get_directions(),
+            "Refill": self.get_refill(),
+        }
+
+
+if __name__ == "__main__":
+    test = """Dr John Smith, M.D
+2 Non-Important Street,
+New York, Phone (000)-111-2222
+
+Name: Marta Sharapova Date: 9/11/2022
+Address: 9 tennis court, new Russia, DC
+Prednisone 20 mig
+Lialda 2.4 gram
+
+Directions:
+
+Prednisone, Taper 5 mg every 3 days,
+Finish in 2.5 weeks a
+Lialda - take 2 pill everyday for 1 month >
+
+Refill: 2 times"""
+    pp = prescription_parser(test)
+    print(pp.parse())
