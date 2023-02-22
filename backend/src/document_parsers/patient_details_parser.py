a748e26a2e5348e4be6323fb0255c984d8f0397c
vinicius <vinicius.savordelli@gmail.com>
2023-02-22 18:38:49 -0300

    fix(flake8)

--- backend/src/document_parsers/patient_details_parser.py
+++ backend/src/document_parsers/patient_details_parser.py
@@ -1,22 +1,26 @@
-from sr.document_parsers.generic_parser import GenericParser
+from generic_parser import GenericParser
 
 
 class PatientDetailsParser(GenericParser):
     def __init__(self, text):
         self.text = text
         self.patterns = {
-            "issue_date": "^(.*)\n",
-            "patient_name": "Birth Date.*?\n(?:.*\n)*?(.+)(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)",
-            "patient_birth_date": "[a-zA-Z]{3}\s[0-1]?[1-9]\s[1-9]{4}",
-            "patient_phone_number": "\([1-9]{3}\)\s?.{7,8}",
-            "patient_address": "Weight.+?[\r\n](.*)Height",
-            "patient_weight": "Weight.+?\r?\n[^\r\n]+\s+(\S+)\r?\n",
-            "patient_height": "^.*Height.*?(\d{2,3})",
-            "emergency_contact": "(?<=Emergency\s..)[\s\S]*?(?=Gener.+ Medi.+ His.+ )",
-            "chicken_pox": "Meas.es:[^\n]*\n(?:.*\n)*?.*?\s?(IMMUNE|NOT IMMUNE)",
-            "measles": "Meas.es:[^\n]*\n(?:.*\n)*?.*?\s+.*?\s(IMMUNE|NOT IMMUNE)",
-            "hepatitis_b_vaccitation": "Hepa.+\?\s+\n?.+\s?.?\s?(Yes|yes|No|no|Positive|positive|Negative|negative)",
-            "medical_problems": "Medical Problems.*?\n(?:.*\n)*?.*?\s?(.*)",
+            "issue_date": r"^(.*)\n",
+            "patient_name": r"Birth Date.*?\n(?:.*\n)*?.*?\s?(.*)?"
+            + r"(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)",
+            "patient_birth_date": r"[a-zA-Z]{3}\s[0-1]?[1-9]\s[1-9]{4}",
+            "patient_phone_number": r"\([1-9]{3}\)\s?.{7,8}",
+            "patient_address": r"Weight.+?[\r\n](.*)Height",
+            "patient_weight": r"Weight.+?\r?\n[^\r\n]+\s+(\S+)\r?\n",
+            "patient_height": r"^.*Height.*?(\d{2,3})",
+            "emergency_contact": r"Emergency[\s\S]*?(?=Gener.+ Medi.+ His.+ )",
+            "chicken_pox": r"Meas.es:[^\n]*\n(?:.*\n)*?.*?\s?"
+            + r"(IMMUNE|NOT IMMUNE)",
+            "measles": r"Meas.es:[^\n]*\n(?:.*\n)*?.*?\s+.*?\s"
+            + r"(IMMUNE|NOT IMMUNE)",
+            "hepatitis_b_vaccitation": r"Hepa.+\?\s+\n?.+\s?.?\s?"
+            + r"(Yes|yes|No|no|Positive|positive|Negative|negative)",
+            "medical_problems": r"Medical Problems.*?\n(?:.*\n)*?.*?\s?(.*)",
         }
 
     def parse(self):
@@ -53,87 +57,7 @@ class PatientDetailsParser(GenericParser):
 
 # Months = Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec
 if __name__ == "__main__":
-    test = """17/12/2020
+    test = """"""
 
- 
-
-Patient Medical Record
-
- 
-
- 
-
-Patient Information Birth Date
-Kathy Crawford May 6 1972
-(737) 988-0851 Weight’
-9264 Ash Dr 95
-New York City, 10005 '
-United States Height:
-190
-In Casc of Emergency
-a _
-Simeone Crawford 9266 Ash Dr
-H New York City, New York, 10005
-ome phone United States
-(990) 375-4621
-Work phone
-Genera! Medical History
-LT
-
-nn
-ch LT a
-
-Chicken Pox (Varicella): Measies:
-
-IMMUNE IMMUNE
-
-Have you had the Hepatitis B vaccination?
-
-No
-
-List any Medical Problems (asthma, seizures, headaches):
-
-Migraine"""
-
-    test2 = """17/12/2020
-
-Patient Medical Record
-
-Patient Information Birth Date
-
-Jerry Lucas May 2 1998
-
-(279) 920-8204 . Weight:
-
-4218 Wheeler Ridge Dr 57
-
-Buffalo, New York, 14201 Height:
-
-United States gn
-170
-
-In Case of Emergency
-meee
-
-Joe Lucas 4218 Wheeler Ridge Dr
-Buffalo, New York, 14201
-Home phone . United States
-Work phone
-
-General Medical History
-
- 
-
-Chicken Pox (Varicelia): Measles:
-
-IMMUNE NOT IMMUNE
-
-Have you had the Hepatitis B vaccination?
-
-_ Yes
-
-List any Medical Problems (asthma, seizures, headaches):
-N/A"""
-
-    pd = PatientDetailsParser(test2)
+    pd = PatientDetailsParser(test)
     print(pd.parse())
--- backend/src/document_parsers/prescription_parser.py
+++ backend/src/document_parsers/prescription_parser.py
@@ -1,17 +1,16 @@
 from sr.document_parsers.generic_parser import GenericParser
-import re
 
 
 class PrescriptionParser(GenericParser):
     def __init__(self, text):
         self.text = text
         self.patterns = {
-            "doctor_info": "Dr(.*)Name",
-            "patient_name": "Name:(.*)Date",
-            "address": "Address:(.*)",
-            "prescription": "Address[^\n]*(.*)Directions",
-            "directions": "Directions:(.*)Refill",
-            "refill": "Refill:(.*)",
+            "doctor_info": r"Dr(.*)Name",
+            "patient_name": r"Name:(.*)Date",
+            "address": r"Address:(.*)",
+            "prescription": r"Address[^\n]*(.*)Directions",
+            "directions": r"Directions:(.*)Refill",
+            "refill": r"Refill:(.*)",
         }
 
     def parse(self):
@@ -28,27 +27,6 @@ class PrescriptionParser(GenericParser):
 
 
 if __name__ == "__main__":
-    test = """Dr John Smith, M.D
-2 Non-Important Street,
-New York, Phone (000)-111-2222
-
-Name: Marta Sharapova Date: 5/11/2022
-
-Address: 9 tennis court, new Russia, DC
-
-K
-
- 
-
-Prednisone 20 mg
-Lialda 2.4 gram
-
-Directions:
-Prednisone, Taper 5 mg every 3 days,
-
-Finish in 2.45 weeks |
-Ltalda - take 2 pill everyday for 1 month
-
-Refill: 2 times"""
+    test = """"""
     pp = PrescriptionParser(test)
     print(pp.parse())
